36570eef63c36d413166248f51fc28fe8a32d7cb
xogns0604 <xogns0604@naver.com>
2022-11-05 21:53:16 +0900

    UPDATE
    UPDATE

---
+++ codingTest/src/LV2/minimum/Run.java
@@ -0,0 +1,14 @@
+package LV2.minimum;
+//https://school.programmers.co.kr/learn/courses/30/lessons/12941
+
+
+public class Run {
+
+	public static void main(String[] args) {
+		int []A = {1, 4, 2};
+		int []B = {5, 4, 4};
+		
+		System.out.println(new Solution().solution(A, B));
+	}
+
+}
